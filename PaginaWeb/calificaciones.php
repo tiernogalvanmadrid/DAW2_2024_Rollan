6c85e9d793cbdbed07b9981dbed2314b2f97bc3c
CristinaRollan <144910767+CristinaRollan@users.noreply.github.com>
2024-05-10 23:02:56 +0200

    Delete PaginaWeb directory

--- PaginaWeb/calificaciones.php
+++
@@ -1,9 +0,0 @@
-<?php include 'header.php'; ?>
-
-<section>
-    <h1>Calificaciones</h1>
-    <p>Contenido de calificaciones en formato texto.</p>
-    <p>Calificación: 9.5/10</p>
-</section>
-
-<?php include 'footer.php'; ?>
--- PaginaWeb/index.php
+++
@@ -1,6 +0,0 @@
-<?php include 'header.php'; ?>
-    <section>
-        <h1>Pagina web con php</h1>
-        <p>mi primera pagina web</p>
-    </section>
-<?php include 'footer.php'; ?>
